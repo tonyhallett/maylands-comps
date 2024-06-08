2f6204371c7f63b8974e19e8fe1a48df8e6ae07c
Tony Hallett <tonyhallett74@gmail.com>
2024-06-08 15:25:32 +0100

    more umpire tests

--- __tests__/umpire.test.ts
+++ __tests__/umpire.test.ts
@@ -1,8 +1,9 @@
-import { getPlayers, getUmpire } from "../src/umpire/index";
+import { Player, TeamScore, getPlayers, getUmpire } from "../src/umpire/index";
 describe("umpiring", () => {
   const singlesPlayers = getPlayers(false);
   const doublesPlayers = getPlayers(true);
   const getAnUmpire = (bestOf = 1) => getUmpire("normal", false, bestOf);
+  const getNormalSinglesBestOf5Umpire = () => getUmpire("normal", false, 5);
 
   describe("initialization", () => {
     it("should have team1 left", () => {
@@ -65,36 +66,160 @@ describe("umpiring", () => {
 
   describe("setting initial server", () => {
     describe("when singles", () => {
-      // todo parameterize
-      it("should set the server and receiver", () => {
-        const umpire = getUmpire("normal", false, 1);
-        umpire.setInitialServer("Team1Player1");
-        expect(umpire.availableServers).toHaveLength(0);
-        expect(umpire.availableReceivers).toHaveLength(0);
-        expect(umpire.server).toEqual("Team1Player1");
-        expect(umpire.receiver).toEqual("Team2Player1");
-      });
+      const serverReceivers: Array<[Player, Player]> = [
+        ["Team1Player1", "Team2Player1"],
+        ["Team2Player1", "Team1Player1"],
+      ];
+      it.each(serverReceivers)(
+        "should set the server and receiver",
+        (server, expectedReceiver) => {
+          const umpire = getUmpire("normal", false, 1);
+          umpire.setInitialServer(server);
+          expect(umpire.availableServers).toHaveLength(0);
+          expect(umpire.availableReceivers).toHaveLength(0);
+          expect(umpire.server).toEqual(server);
+          expect(umpire.receiver).toEqual(expectedReceiver);
+        },
+      );
     });
     describe("when doubles", () => {
-      // todo parameterize
-      it("should set the server and available receivers", () => {
-        const umpire = getUmpire("normal", true, 1);
-        umpire.setInitialServer("Team1Player1");
-        expect(umpire.availableServers).toHaveLength(0);
-        expect(umpire.availableReceivers).toEqual([
-          "Team2Player1",
-          "Team2Player2",
-        ]);
-        expect(umpire.server).toEqual("Team1Player1");
-        expect(umpire.receiver).toBeUndefined();
-      });
+      const serverReceivers: Array<[Player, [Player, Player]]> = [
+        ["Team1Player1", ["Team2Player1", "Team2Player2"]],
+        ["Team1Player2", ["Team2Player1", "Team2Player2"]],
+        ["Team2Player1", ["Team1Player1", "Team1Player2"]],
+        ["Team2Player2", ["Team1Player1", "Team1Player2"]],
+      ];
+
+      it.each(serverReceivers)(
+        "should set the server and available receivers",
+        (server, expectedReceivers) => {
+          const umpire = getUmpire("normal", true, 1);
+          umpire.setInitialServer(server);
+          expect(umpire.availableServers).toHaveLength(0);
+          expect(umpire.availableReceivers).toEqual(expectedReceivers);
+          expect(umpire.server).toEqual(server);
+          expect(umpire.receiver).toBeUndefined();
+        },
+      );
     });
   });
 
-  it("should allow switching ends", () => {
-    // should happen before match starts
-    const umpire = getAnUmpire();
-    umpire.switchEnds();
-    expect(umpire.team1Left).toBe(false);
+  describe("scoring", () => {
+    // todo - that game scores are kept !
+    it.each([true, false])("should increment points", (team1Scores) => {
+      const umpire = getAnUmpire();
+      umpire.pointScored(team1Scores);
+      const incrementedTeamScore = team1Scores
+        ? umpire.team1Score
+        : umpire.team2Score;
+      const notIncrementedTeamScore = team1Scores
+        ? umpire.team2Score
+        : umpire.team1Score;
+      expect(incrementedTeamScore).toEqual<TeamScore>({
+        gamesWon: 0,
+        pointsWon: 1,
+      });
+      expect(notIncrementedTeamScore).toEqual<TeamScore>({
+        gamesWon: 0,
+        pointsWon: 0,
+      });
+    });
+    it("should increment games resetting points when game won", () => {
+      const umpire = getNormalSinglesBestOf5Umpire();
+      umpire.pointScored(false);
+      [...Array(11)].forEach(() => umpire.pointScored(true));
+      expect(umpire.team1Score).toEqual<TeamScore>({
+        gamesWon: 1,
+        pointsWon: 0,
+      });
+      expect(umpire.team2Score).toEqual<TeamScore>({
+        gamesWon: 0,
+        pointsWon: 0,
+      });
+    });
+    it("should reset handicap start game scores correctly", () => {
+      // up to 31
+      const umpire = getUmpire("handicap", false, 3, {
+        team1Handicap: -1,
+        team2Handicap: -2,
+        shiftNegatives: false,
+      });
+      umpire.pointScored(true);
+      [...Array(33)].forEach(() => umpire.pointScored(false));
+      expect(umpire.team1Score).toStrictEqual({ gamesWon: 0, pointsWon: -1 });
+      expect(umpire.team2Score).toStrictEqual({ gamesWon: 1, pointsWon: -2 });
+    });
+  });
+
+  describe("switching ends", () => {
+    it("should allow switching ends", () => {
+      // should happen before match starts
+      const umpire = getAnUmpire();
+      umpire.switchEnds();
+      expect(umpire.team1Left).toBe(false);
+    });
+
+    it("should switch ends after game", () => {
+      const umpire = getNormalSinglesBestOf5Umpire();
+      expect(umpire.team1Left).toBe(true);
+      [...Array(10)].forEach(() => {
+        umpire.pointScored(true);
+        expect(umpire.team1Left).toBe(true);
+      });
+
+      umpire.pointScored(true);
+      expect(umpire.team1Left).toBe(false);
+    });
+
+    it("should switch ends in the middle of the last game", () => {
+      const umpire = getNormalSinglesBestOf5Umpire();
+      expect(umpire.team1Left).toBe(true);
+      [...Array(22)].forEach(() => umpire.pointScored(true));
+      expect(umpire.team1Left).toBe(true);
+      [...Array(22)].forEach(() => umpire.pointScored(false));
+      expect(umpire.team1Left).toBe(true);
+      [...Array(5)].forEach(() => umpire.pointScored(true));
+      expect(umpire.team1Left).toBe(false);
+    });
+
+    it("should only switch ends once in the middle of the last game", () => {
+      const umpire = getNormalSinglesBestOf5Umpire();
+      expect(umpire.team1Left).toBe(true);
+      [...Array(22)].forEach(() => umpire.pointScored(true));
+      expect(umpire.team1Left).toBe(true);
+      [...Array(22)].forEach(() => umpire.pointScored(false));
+      expect(umpire.team1Left).toBe(true);
+      [...Array(5)].forEach(() => umpire.pointScored(true));
+      expect(umpire.team1Left).toBe(false);
+      [...Array(5)].forEach(() => umpire.pointScored(false));
+      expect(umpire.team1Left).toBe(false);
+    });
+
+    it("should switch ends in the last handicap game differently for each player", () => {
+      const umpire = getUmpire("handicap", false, 3, {
+        team1Handicap: 10,
+        team2Handicap: 20,
+        shiftNegatives: false,
+      });
+      expect(umpire.team1Left).toBe(true);
+      [...Array(21)].forEach(() => umpire.pointScored(true));
+      [...Array(11)].forEach(() => umpire.pointScored(false));
+      expect(umpire.team1Left).toBe(true);
+
+      throw new Error("finish this test");
+    });
+
+    it("should not switch ends when the match is won", () => {
+      const umpire = getNormalSinglesBestOf5Umpire();
+      expect(umpire.team1Left).toBe(true);
+      [...Array(33)].forEach(() => umpire.pointScored(true));
+
+      expect(umpire.team1Left).toBe(true);
+    });
+  });
+
+  describe("serving", () => {
+    // do singles 2 serves /  5 serves
+    // keep track of number of serves remaining too
   });
 });
--- src/umpire/index.ts
+++ src/umpire/index.ts
@@ -30,8 +30,12 @@ interface HandicapOptions {
 }
 
 type PossibleHandicapOptions = HandicapOptions | undefined;
-type Player = "Team1Player1" | "Team1Player2" | "Team2Player1" | "Team2Player2";
-interface TeamScore {
+export type Player =
+  | "Team1Player1"
+  | "Team1Player2"
+  | "Team2Player1"
+  | "Team2Player2";
+export interface TeamScore {
   gamesWon: number;
   pointsWon: number;
 }
@@ -66,7 +70,7 @@ export function getPlayers(isDoubles: boolean): Player[] {
 export function getUmpire(
   competition: Competitions,
   isDoubles: boolean,
-  bestOf: number,
+  bestOf: number, // todo allow fixed number of games / unlimited
   handicapOptions?: PossibleHandicapOptions,
 ): Umpire {
   return new Umpire(
@@ -77,30 +81,13 @@ export function getUmpire(
   );
 }
 
+enum GameWonState {
+  NotWon,
+  Team1Won,
+  Team2Won,
+}
+
 export class Umpire {
-  setInitialServer(player: Player): void {
-    if (!this._availableServers.includes(player)) {
-      throw new Error("Player not available");
-    }
-    this._availableServers = [];
-    this._server = player;
-    if (this.isDoubles) {
-      const isTeam1 = player === "Team1Player1" || player === "Team1Player2";
-
-      this._availableReceivers = isTeam1
-        ? ["Team2Player1", "Team2Player2"]
-        : ["Team1Player1", "Team1Player2"];
-
-      this._receiver = undefined;
-    } else {
-      this._receiver =
-        player === "Team1Player1" ? "Team2Player1" : "Team1Player1";
-    }
-  }
-  switchEnds(): void {
-    this._team1Left = !this._team1Left;
-  }
-
   private team1StartGameScore = 0;
   private team2StartGameScore = 0;
 
@@ -139,6 +126,8 @@ export class Umpire {
   public get team2Score(): Readonly<TeamScore> {
     return { ...this._team2Score };
   }
+
+  private switchedEndsInLastGame = false;
 
   constructor(
     private umpireOptions: CompetitionScoringOptions,
@@ -185,5 +174,102 @@ export class Umpire {
       this._team2Score.pointsWon = this.team2StartGameScore;
       this._upTo = this.umpireOptions.upTo + shift;
     }
+  }
+
+  setInitialServer(player: Player): void {
+    if (!this._availableServers.includes(player)) {
+      throw new Error("Player not available");
+    }
+    this._availableServers = [];
+    this._server = player;
+    if (this.isDoubles) {
+      const isTeam1 = player === "Team1Player1" || player === "Team1Player2";
+
+      this._availableReceivers = isTeam1
+        ? ["Team2Player1", "Team2Player2"]
+        : ["Team1Player1", "Team1Player2"];
+
+      this._receiver = undefined;
+    } else {
+      this._receiver =
+        player === "Team1Player1" ? "Team2Player1" : "Team1Player1";
+    }
+  }
+
+  switchEnds(): void {
+    this._team1Left = !this._team1Left;
+  }
+
+  pointScored(team1: boolean): void {
+    const teamScore = team1 ? this._team1Score : this._team2Score;
+    teamScore.pointsWon += 1;
+    this.switchEndsIfMidwayLastGame();
+    const gameWonState = this.getGameWonState();
+    if (gameWonState === GameWonState.NotWon) {
+      return;
+    }
+    this.nextGame(gameWonState === GameWonState.Team1Won);
+  }
+
+  private switchEndsIfMidwayLastGame(): void {
+    if (!this.switchedEndsInLastGame && this.isMidwayLastGame()) {
+      this.switchEnds();
+      this.switchedEndsInLastGame = true;
+    }
+  }
+
+  private isMidwayLastGame(): boolean {
+    return this.isLastGame() && this.isMidwayGame();
+  }
+
+  private isMidwayGame(): boolean {
+    const team1PointsToWin = this._upTo - this.team1StartGameScore;
+    const team2PointsToWin = this._upTo - this.team2StartGameScore;
+    const team1MidwayPoints =
+      this.team1StartGameScore + Math.floor(team1PointsToWin / 2);
+    const team2MidwayPoints =
+      this.team2StartGameScore + Math.floor(team2PointsToWin / 2);
+    return (
+      this._team1Score.pointsWon === team1MidwayPoints ||
+      this._team2Score.pointsWon === team2MidwayPoints
+    );
+  }
+
+  private isLastGame(): boolean {
+    return (
+      this._team1Score.gamesWon + this._team2Score.gamesWon === this.bestOf - 1
+    );
+  }
+
+  private getGameWonState(): GameWonState {
+    const team1IsUpTo = this._team1Score.pointsWon === this._upTo;
+    const team2IsUpTo = this._team2Score.pointsWon === this._upTo;
+    const isUpTo = team1IsUpTo || team2IsUpTo;
+    if (!isUpTo) {
+      return GameWonState.NotWon;
+    }
+    const pointsDifference = Math.abs(
+      this._team1Score.pointsWon - this._team2Score.pointsWon,
+    );
+    const clearBy = this.umpireOptions.clearBy2 ? 2 : 1;
+    if (pointsDifference < clearBy) {
+      return GameWonState.NotWon;
+    }
+    return team1IsUpTo ? GameWonState.Team1Won : GameWonState.Team2Won;
+  }
+
+  private nextGame(team1Won: boolean): void {
+    const teamScore = team1Won ? this._team1Score : this._team2Score;
+    teamScore.gamesWon += 1;
+    this._team1Score.pointsWon = this.team1StartGameScore;
+    this._team2Score.pointsWon = this.team2StartGameScore;
+
+    if (teamScore.gamesWon !== this.requiredGamesToWin()) {
+      this.switchEnds();
+    }
+  }
+
+  private requiredGamesToWin(): number {
+    return Math.ceil(this.bestOf / 2);
   }
 }
